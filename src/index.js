b967a74d2d3333c0afe7d03cc155690a8204d956
Peng Wu <peng116233402@gmail.com>
2017-04-03 11:47:53 -0600

    move files to correct path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './presentational/App';
+import './presentational/index.css';
 
 ReactDOM.render(
   <App />,
