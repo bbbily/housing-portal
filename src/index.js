2e9013a7c388af7f90a81c869977382f457646ef
Peng Wu <peng116233402@gmail.com>
2017-04-03 12:39:28 -0600

    set reducer and action test

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './presentational/App';
 import './presentational/index.css';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxPromise from "redux-promise";
+import reducers from "./reducers";
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={ createStoreWithMiddleware(reducers)} >
+    <App />
+
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/reducers/reducer_apartments.js
@@ -0,0 +1,8 @@
+// import { FETCH_APARTMENTS } from "../actions/action_apartment";
+
+export default function(state={}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
