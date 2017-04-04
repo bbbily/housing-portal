8b65619740afdad6db1b219a089a466d8b9409c5
Peng Wu <peng116233402@gmail.com>
2017-04-04 09:51:25 -0600

    form test

--- src/actions/action_apartments.js
+++ src/actions/action_apartments.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const FETCH_APARTMENTS = "FETCH_APARTMENTS";
+export const CREATE_APARTMENT = "CREATE_APARTMENT";
 
 const ROOT_URL = "http://swapi.co/api/people/";
 
@@ -10,6 +11,17 @@ export function fetchApartments() {
 
   return {
     type: FETCH_APARTMENTS,
+    payload: request
+  }
+}
+
+export function createApartment(props) {
+  const url = `${ROOT_URL}`;
+  const request = axios.post(url, props)
+  console.log(props);
+
+  return {
+    type: CREATE_APARTMENT,
     payload: request
   }
 }
