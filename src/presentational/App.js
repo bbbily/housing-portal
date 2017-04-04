eb89adf7551f057b564a021e63665d4c0f98eca0
Peng Wu <peng116233402@gmail.com>
2017-04-04 11:53:10 -0600

    add routes

--- src/presentational/App.js
+++ src/presentational/App.js
@@ -1,12 +1,9 @@
 
 import React, { Component } from 'react';
-import DatePickerComponent from "../DatePicker";
 import StudentForm from "../containers/StudentForm";
 import Test from "../containers/test.js"
 import '../styles/App.css';
 import DatePickerComponent from "../containers/DatePicker";
-
->>>>>>> master
 
 
 class App extends Component {
