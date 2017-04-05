3b70347091ac9ddef793b2f0a312d9a0113bc8b8
Jeremy Gray <jeremygray001@gmail.com>
2017-04-04 19:35:41 -0600

    Styled modal

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ import Admin from "./presentational/views/Admin/Admin";
 // import NavBar from "./presentational/NavBar";
 // import Student from "./presentational/views/Student";
 // import DropDown from "./containers/DropDownMenu";
-// import ApartmentListFilter from "./containers/ApartmentListFilter";
-// import StudentListFilter from "./containers/StudentListFilter";
+ import ApartmentListFilter from "./containers/ApartmentListFilter";
+ import StudentListFilter from "./containers/StudentListFilter";
 import CreateNewAdmin from "./presentational/views/Admin/CreateNewAdmin";
 import CreateNewCohort from "./presentational/views/Admin/CreateNewCohort";
 import DatePickerComponent from "./containers/DatePicker";
@@ -27,8 +27,8 @@ ReactDOM.render(
     <Router history={ browserHistory } >
       <Route path="/" component={ App } />
       <Route path="/admin" component={Admin} />
-      <Route path="/apartmentList" component={ApartmentList} />
-      <Route path="/studentList" component={StudentList} />
+      {/*<Route path="/apartmentList" component={ApartmentList} />
+      <Route path="/studentList" component={StudentList} />*/}
     </Router>
   </Provider>,
   document.getElementById('root')
