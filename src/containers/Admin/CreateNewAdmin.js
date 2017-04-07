2cbbe03a9083d64f96e68b5d1bf6cb707f08dc79
Jeremy Gray <jeremygray001@gmail.com>
2017-04-07 17:54:54 -0600

    Style changes in Cohorts panel on Admin page

--- src/containers/Admin/CreateNewAdmin.js
+++ src/containers/Admin/CreateNewAdmin.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Panel } from "react-bootstrap";
+import { Panel, Button } from "react-bootstrap";
 import { getAdmins, createAdmin } from "../../actions/action_admin";
 import { connect } from "react-redux";
 import DropDown from "../DropDownMenu";
@@ -32,7 +32,7 @@ class CreateNewAdmin extends Component {
         return (
             <div className="new-admin-container">
                 <div onClick={ ()=> this.setState({ open: !this.state.open }) }>
-                    <h3>New Admin</h3>
+                    <h3>Admin Users</h3>
                 </div>
                 <Panel collapsible expanded={ this.state.open }>
                     <div className="users-panel">
@@ -46,14 +46,18 @@ class CreateNewAdmin extends Component {
                         <ul>
                           { admins }
                         </ul>
-                        <h4>New User:</h4>
-                        <input type="text" placeholder="firstName" value={ this.state.first_name } onChange={ e => { this.setState({first_name: e.target.value})} }/>
-                        <input type="text" placeholder="lastName" value={ this.state.last_name } onChange={ e => { this.setState({last_name: e.target.value})} }/>
-                        <input type="text" placeholder="Email" value={ this.state.email } onChange={ e => { this.setState({email: e.target.value})} } />
-                        <input type="password" placeholder="Password" value={ this.state.password } onChange={ e => this.setState({password: e.target.value})}  />
-                        <button onClick={ () => this.props.dispatch(createAdmin(this.state))}>Save</button>
-                        <button>Cancel</button>
+                    <ul className="admin-user-title">
+                        <li><h4>New Admin</h4></li>
+                    </ul>
+                    <ul className="admin-user-title">
+                        <li><input type="text" className="admin-input-text" placeholder="first" value={ this.state.first_name } onChange={ e => { this.setState({first_name: e.target.value})} }/></li>
+                        <li><input type="text" className="admin-input-text" placeholder="last" value={ this.state.last_name } onChange={ e => { this.setState({last_name: e.target.value})} }/></li>
+                        <li><input type="text" className="admin-input-text" placeholder="email" value={ this.state.email } onChange={ e => { this.setState({email: e.target.value})} } /></li>
+                        <li><input type="password" className="admin-input-text" placeholder="Password" value={ this.state.password } onChange={ e => this.setState({password: e.target.value})}  /></li>
+                        <li><Button onClick={ () => this.props.dispatch(createAdmin(this.state))}>Save</Button></li>
+                    </ul>
                     </div>
+
                 </Panel>
             </div>
         );
