add62dee41177f96c4df317a68ccebdba8edb7a1
Jeremy Gray <jeremygray001@gmail.com>
2017-04-08 17:03:26 -0600

    New Student panel complete.

--- src/containers/Admin/CreateNewStudent.js
+++ src/containers/Admin/CreateNewStudent.js
@@ -6,6 +6,9 @@ import "../../styles/CreateNewStudent.scss"
 import { getCohorts, getCampuses} from "../../actions/action_admin"
 import moment from 'moment'
 
+
+
+
 class CreateNewStudent extends Component {
   constructor(...args) {
     super(...args)
@@ -33,16 +36,32 @@ class CreateNewStudent extends Component {
       accomodations: null,
       notes: null,
       room_id: null,
-      
     }
 
     this.handleInputChange = this.handleInputChange.bind(this)
-    this.createStudent = this.createStudent.bind(this)
+    this.clearData = this.clearData.bind(this)
   }
   
-  sendStudent(studentObj) {
-    this.props.dispatch(createStudent(studentObj))
-    console.log(studentObj)
+  clearData() {
+    this.refs.first_name.value="",
+    this.refs.last_name.value="",
+    this.refs.dob.value="",
+    this.refs.arrive_date.value="",
+    this.refs.leave_date.value="",
+    this.refs.email.value="",
+    this.refs.slack.value="",
+    this.refs.phone.value="",
+    this.refs.street_address.value="",
+    this.refs.city.value="",
+    this.refs.state.value="",
+    this.refs.country.value="",
+    this.refs.post_code.value="",
+    this.setState({
+      gender: null,
+      campus: null,
+      cohort: null,
+      deposit: null,
+    })
   }
 
   handleInputChange(event) {
@@ -55,18 +74,9 @@ class CreateNewStudent extends Component {
     })
   }
 
-  componentWillMount() {
+  componentDidMount() {
         this.props.dispatch(getCohorts())
         this.props.dispatch(getCampuses()) 
-    }
-
-    handleSelect(evt, evtKey) {
-      console.log("evt: ",evt)
-      console.log("evtKey: ",evtKey)
-    }
-
-    createStudent() {
-      console.log(this.state)
     }
 
 
@@ -91,20 +101,22 @@ class CreateNewStudent extends Component {
           <h3>New Student</h3>
         </div>
         <Panel collapsible expanded={ this.state.open }>
+        <form ref="new_student_form">
           <div className="students-panel">
             <div className="row">
                 <div className="col-sm-6">
                   <h1>General</h1>
                 <ul className="student-ul">
-                  <li><input type="text" placeholder="First" name="first_name" className="student-40" onChange={this.handleInputChange}/></li>
-                  <li><input type="text" placeholder="Last" name="last_name"  className="student-40" onChange={this.handleInputChange}/></li>
-                  <li>DOB <br/> <input type="date" name="dob"  className="student-20" onChange={this.handleInputChange}/> </li>
+              
+                  <li><input type="text" placeholder="First" name="first_name" ref="first_name" className="student-40" onChange={this.handleInputChange}/></li>
+                  <li><input type="text" placeholder="Last" name="last_name" ref="last_name" className="student-40" onChange={this.handleInputChange}/></li>
+                  <li>DOB <br/> <input type="date" name="dob" ref="dob" className="student-20" onChange={this.handleInputChange}/> </li>
                   <li>
                     <div className="item-container">
                       <div className="item-left">                    
                         <DropdownButton title="Gender" onSelect={ evt => { this.setState({ gender: evt }) }}  className="student-20">
-                          <MenuItem eventKey="Male">Male</MenuItem>
-                          <MenuItem eventKey="Female">Female</MenuItem>
+                          <MenuItem eventKey="M">Male</MenuItem>
+                          <MenuItem eventKey="F">Female</MenuItem>
                         </DropdownButton>
                       </div>
                         <div className="item-right">{this.state.gender}</div>
@@ -142,11 +154,11 @@ class CreateNewStudent extends Component {
                         <div className="item-right">{this.state.deposit_paid}</div>
                     </div>
                   </li>
-                  <li>Arrives:<input type="date" name="arrive_date" className="student-20" 
+                  <li>Arrives:<input type="date" name="arrive_date" ref="arrive_date" className="student-20" 
                                           value={this.state.arrive_date}
                                           onChange={ evt => {this.setState({ arrive_date: evt.target.value})}}
                                           /></li>
-                  <li>Leaves: <input type="date" name="leave_date" className="student-20" 
+                  <li>Leaves: <input type="date" name="leave_date" ref="leave_date" className="student-20" 
                                           value={this.state.leave_date}
                                           onChange={ evt => { this.setState({ leave_date: evt.target.value})}}
                                           /></li>
@@ -157,19 +169,28 @@ class CreateNewStudent extends Component {
                
                 <h1>Contact</h1>
                 <ul className="student-ul">
-                  <li><input type="text" placeholder="Email" name="email" className="student-40" onChange={this.handleInputChange}/></li>
-                  <li><input type="text" placeholder="Slack" name="slack" className="student-40" onChange={this.handleInputChange}/></li>
-                  <li><input type="text" placeholder="Phone" name="phone" className="student-40" onChange={this.handleInputChange}/></li>
-                  <li><input type="text" placeholder="address" name="street_address" className="student-40" onChange={this.handleInputChange}/></li>
-                  <li><input type="text" placeholder="city" name="city" className="student-30" onChange={this.handleInputChange}/>
-                      <input type="text" placeholder="state" name="state"  className="student-10" onChange={this.handleInputChange}/> </li>
-                  <li><input type="text" placeholder="country" name="country"  className="student-30" onChange={this.handleInputChange}/>
-                      <input type="text" placeholder="zip" name="post_code"  className="student-10" onChange={this.handleInputChange}/></li>
+                  <li><input type="text" placeholder="Email" name="email" ref="email" className="student-40" onChange={this.handleInputChange}/></li>
+                  <li><input type="text" placeholder="Slack" name="slack" ref="slack" className="student-40" onChange={this.handleInputChange}/></li>
+                  <li><input type="text" placeholder="Phone" name="phone" ref="phone" className="student-40" onChange={this.handleInputChange}/></li>
+                  <li><input type="text" placeholder="address" name="street_address" ref="street_address" className="student-40" onChange={this.handleInputChange}/></li>
+                  <li><input type="text" placeholder="city" name="city" ref="city" className="student-30" onChange={this.handleInputChange}/>
+                      <input type="text" placeholder="state" name="state" ref="state" className="student-10" onChange={this.handleInputChange}/> </li>
+                  <li><input type="text" placeholder="country" name="country" ref="country" className="student-30" onChange={this.handleInputChange}/>
+                      <input type="text" placeholder="zip" name="post_code" ref="post_code" className="student-10" onChange={this.handleInputChange}/></li>
                   <ul className="student-ul">
-                    <li><Button onClick={this.createStudent}>     Add Student     </Button></li>
+                    <li><Button onClick={ () => 
+                      { this.props.dispatch(createStudent(this.state))
+                        this.clearData()
+                        this.setState({open: false})
+                        alert(this.state.first_name + this.state.last_name + " added.")
+                      }}>     Add Student     </Button></li>
+                    <li><Button onClick={ () =>
+                      { this.clearData() }}>Clear</Button>
+                    </li>
+
                   </ul>
                 
-                </ul>
+                </ul> 
 
               </div>
               </div>
@@ -184,11 +205,12 @@ class CreateNewStudent extends Component {
                   <li><input type="text" placeholder="car info" name="car_info" className="student-80" onChange={this.handleInputChange}/></li>
                 </ul>
                 <ul className="student-ul">
-                  <li><Button onClick={this.createStudent}>     Add Student     </Button></li>
+                  <li><Button onClick={ () => {this.props.dispatch(createStudent(this.state))}}>     Add Student     </Button></li>
                 </ul>
                 </div>
               </div>
           </div>
+        </form>
         </Panel>
     </div>
 
