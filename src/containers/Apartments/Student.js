4b96eb91fa4953b4c07281069cc7a5ee6a9263f5
Paige Busch <93paigedf@gmail.com>
2017-04-11 12:54:41 -0600

    styled dnd student cards

--- src/containers/Apartments/Student.js
+++ src/containers/Apartments/Student.js
@@ -1,15 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 import { DragSource } from 'react-dnd';
 import ItemTypes from './itemTypes';
+import "../../styles/aptstudentcard.scss";
 
-const style = {
-  border: '1px dashed gray',
-  backgroundColor: 'white',
-  padding: '0.5rem 1rem',
-  marginRight: '1.5rem',
-  marginBottom: '1.5rem',
-  float: 'left',
-};
+// const style = {
+//   border: '1px dashed gray',
+//   backgroundColor: 'white',
+//   padding: '0.5rem 1rem',
+//   marginRight: '1.5rem',
+//   marginBottom: '1.5rem',
+//   float: 'left',
+// };
 
 const StudentSource = {
   beginDrag(props) {
@@ -55,7 +56,8 @@ class Student extends Component {
 
     return (
       connectDragSource(
-        <div style={{ ...style, opacity }}>
+        <div className="aptcard-button">
+          <img src="https://s3.amazonaws.com/37assets/svn/1065-IMG_2529.jpg" className="aptcard-image" />
           {name}
         </div>,
       )
