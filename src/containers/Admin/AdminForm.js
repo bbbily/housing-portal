12e824167c5696dba6139c4a502ad1171502ae4f
Jeremy Gray <jeremygray001@gmail.com>
2017-04-19 16:31:40 -0600

    Fixed edit button on Admin Users panel

--- src/containers/Admin/AdminForm.js
+++ src/containers/Admin/AdminForm.js
@@ -3,7 +3,6 @@ import { Field, reduxForm } from "redux-form";
 import {MenuItem, DropdownButton} from 'react-bootstrap'
 import { editAdmin, deleteAdmin } from "../../actions/action_admin";
 import "../../styles/admin.scss"
-import edit from "../../styles/icons/edit.svg"
 
 
 class AdminForm extends Component {
@@ -29,15 +28,15 @@ class AdminForm extends Component {
 
         <ul className="admin-user-list">
         <li><Field name="first_name" component="input" type="text" className="admin-input-text"/></li>
-           <li><Field name="last_name" component="input" type="text"  className="admin-input-text"/></li>
-           <li><Field name="email" component="input" type="text" value="addddd" className="admin-input-text"/></li>
-           <li><Field name="campus_id" component="select" className="admin-input-text">
+        <li><Field name="last_name" component="input" type="text"  className="admin-input-text"/></li>
+        <li><Field name="email" component="input" type="text" value="addddd" className="admin-input-text"/></li>
+        <li><Field name="campus_id" component="select" className="admin-input-text">
             <option value="1">Provo</option>
             <option value="2">Salt Lake city</option>
             <option value="3">Dallas</option>
           </Field></li>
 
-          <input type="image" src={require("../../styles/icons/save.svg")} name="submit" className="admin-icon" />
+          <input type="image" src={require("../../styles/icons/save.svg")} name="submit" className="edit-icon"/>
           <img src={require("../../styles/icons/garbage.svg")} onClick={ this.handleDelete.bind(this) } className="admin-icon" />
 
 
