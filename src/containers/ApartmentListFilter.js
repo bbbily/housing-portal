bdd5ebb39cd8db44205ed9565a85f5226e7a1fd4
Jeremy Gray <jeremygray001@gmail.com>
2017-04-20 23:15:53 -0600

    Styling updates for apartments list

--- src/containers/ApartmentListFilter.js
+++ src/containers/ApartmentListFilter.js
@@ -21,32 +21,22 @@ class ApartmentListFilter extends Component {
 
     render() {
         return (
+       
             <div className="filter-container">
-                <div>
-                    <Button onClick={ () => this.setState({ open: !this.state.open })}>
-                        Filter Results
-                    </Button>
-                    <Panel collapsible expanded={ this.state.open }>
-                        <div>
-                            Campus
-                            <CampusSelector handleChange={ this.handleChange.bind(this, this.props.campus_id)} />
-                        </div>
-                        <div>
-                            <input type="checkbox" name="over_21" value={ this.props.over_21 } onChange={ this.handleChange.bind(this, !this.props.over_21) } /> 21+
-                        </div>
-                        <div>
-                            <input type="checkbox" name="open" value={ this.props.open } onChange={ this.handleChange.bind(this, !this.props.open)} /> Open
-                        </div>
-                        <div>
-                          <select name="preferred_gender" defaultValue="" onChange={ this.handleChange.bind(this, this.props.preferred_gender) }>
-                              <option value="M">Male</option>
-                              <option value="F">Female</option>
-                              <option value="">Any</option>
-                          </select>
-                        </div>
-                    </Panel>
-                </div>
+                <ul>
+                    <li>Campus <br /><CampusSelector handleChange={ this.handleChange.bind(this, this.props.campus_id)} /></li> 
+                    <li><input type="checkbox" name="over_21" value={ this.props.over_21 } onChange={ this.handleChange.bind(this, !this.props.over_21) } /> 21+ </li>
+                    <li><input type="checkbox" name="open" value={ this.props.open } onChange={ this.handleChange.bind(this, !this.props.open)} /> Open </li>
+                    <li><select name="preferred_gender" defaultValue="" onChange={ this.handleChange.bind(this, this.props.preferred_gender) }>
+                    <option value="M">Male</option>
+                            <option value="F">Female</option>
+                            <option value="">Any</option>
+                    </select></li>
+                
+                </ul>
             </div>
+
+
         )
     }
 }
